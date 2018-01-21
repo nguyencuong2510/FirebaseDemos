646f80e9f9d9425b84dc758b858309f4f4980a0a
NguyenCuong <cuong251095@gmail.com>
2018-01-21 14:29:54 +0700

    done

--- FirebaseDemos/ViewController.swift
+++ FirebaseDemos/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import FirebaseDatabase
+
 
 class ViewController: UIViewController {
 
+    var dbReferent: DatabaseReference?
+    var dbHanldle: DatabaseHandle?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        dbReferent = Database.database().reference()
+        dbReferent?.child("name").childByAutoId().setValue("BBBB")
+        dbReferent?.child("name").childByAutoId().setValue("AAAA")
+        
+        //retrieve data
+//        dbHanldle = dbReferent?.child("name").observe(.childAdded, with: { (snapshot) in
+//            let  name:String?  = snapshot.value as? String
+//            print(name)
+//        })
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FirebaseDemos' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FirebaseDemos
+	pod 'Firebase/Core'
+	pod 'Firebase/Database’
+end
